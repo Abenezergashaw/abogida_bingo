fe1b19a1797e197300af2e4de6572152a9e7e845
cherent tsegaye <abenigas59@gmail.com>
2025-06-09 14:29:32 +0300

    Mysql credintials

--- server.js
+++ server.js
@@ -26,24 +26,24 @@ const wss = new WebSocketServer({ server });
 const bot = new TelegramBot(token, { polling: true });
 
 // MySQL connection setup
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
 // const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "Ab@596919",
-//   database: "bingo",
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0,
 // });
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Ab@596919",
+  database: "bingo",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 //Routes
 app.get("/ping", (req, res) => {
